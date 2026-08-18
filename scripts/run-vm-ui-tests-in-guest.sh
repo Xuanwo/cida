7cd8f943e89affc693a15b731198e5770355aeb4
Xuanwo <github@xuanwo.io>
2026-08-18 16:05:22 +0800

    test: include real history clicks in VM shards

--- scripts/run-vm-ui-tests-in-guest.sh
+++ scripts/run-vm-ui-tests-in-guest.sh
@@ -98,7 +98,7 @@ else
   interaction_filters=(
     'InteractionReproductionTests/test[A-H]' \
     'InteractionReproductionTests/test[I-P]' \
-    'InteractionReproductionTests/test(Record|Recycled|Scrolling|Settings|Staged)' \
+    'InteractionReproductionTests/test(Real|Record|Recycled|Scrolling|Settings|Staged)' \
     'InteractionReproductionTests/testStreaming' \
     'InteractionReproductionTests/test(Submit|Virtual|Window)'
   )
